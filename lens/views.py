a427038c9728c44f2f4fa59aaec2328f3e9a8fa4
Donghwa96 <ensk960405@gmail.com>
2019-07-29 23:22:23 +0900

    처음으로 페이지 배포

---
+++ lens/views.py
@@ -0,0 +1,250 @@
+from django.shortcuts import render, redirect, get_object_or_404, resolve_url
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm # 장고의 기본적인 회원가입 폼. id, password만 확인한다는 한계점.
+from django.urls import reverse_lazy, reverse
+from .forms import CreateUserForm, UploadForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.contrib.auth.models import User
+from django.views.generic.detail import DetailView
+from django.views import View
+from .forms import UserForm, ProfileForm
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import get_user_model
+from django.template.response import TemplateResponse
+from django.utils.http import urlsafe_base64_decode
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm, PasswordChangeForm
+from django.contrib.auth.tokens import default_token_generator
+
+
+
+@login_required   # 메소드에만 적용가능
+def upload(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES) # 대용량인 이미지를 처리해야 하므로 두 매개변수를 넘겨줘야함.
+        if form.is_valid():
+            photo = form.save(commit=False) # photo객체를 가져오긴 하나 DB에 아직 저장하진 않음
+            photo.owner = request.user      # request.user는 로그인한 사용자
+            form.save()
+            return redirect('lens:index')
+
+
+    form = UploadForm()
+    return render(request, 'lens/upload.html', {'form' : form})
+
+
+class IndexView(ListView):
+    # model = Photo 이렇게 해주면 사용자를 안가리고 모든 photo객체가 넘어가게 되므로 아래와 같이 쿼리를 지정해줌.
+    context_object_name = 'user_photo_list' # 템플릿에 전달되는 이름
+    paginate_by = 2 # 사진을 몇개씩 보여줄 것인지 지정.
+
+    def get_queryset(self):
+        user = self.request.user    # 로그인되어있는 사용자
+        return user.photo_set.all().order_by('-pub_date')
+
+
+'''
+class CreateUserView(CreateView):  # 제네릭의 CreateView는 폼하고 연결돼서, 혹은 모델하고 연결돼서 새로운 데이터를 넣을 때 사용.
+    template_name = 'registration/signup.html'     # 회원가입 할 때 띄울 폼 템플릿
+    #form_class = UserCreationForm # id, pw만 받는 폼 클래스
+    form_class = CreateUserForm    # id, email, pw 까지 받는 사용자 정의 폼 클래스
+    success_url = reverse_lazy('create_user_done') # 성공하면 어디로 갈지, create_user_done은 url name
+    # 여기서 reverse가 아닌 reverse_lazy를 사용하는 이유: 제네릭뷰 같은경우 타이밍 문제 때문에 reverse_lazy를 사용해야함
+
+
+class RegisteredView(TemplateView): # 회원가입이 완료된 경우
+    template_name = 'registration/signup_done.html'
+'''
+
+@login_required
+def profile(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    photos = user.photo_set.filter(is_public=True)[:20]
+    context = {"profile_user": user, "photos": photos}
+    return render(request, 'lens/profile.html', context)
+
+
+'''
+class ProfileView(DetailView):
+    context_object_name = 'profile_user' # model로 지정해준 User모델에 대한 객체와 로그인한 사용자랑 명칭이 겹쳐버리기 때문에 이를 지정해줌.
+    model = User
+    template_name = 'kilogram/profile.html'
+'''
+
+class ProfileUpdateView(View): # 간단한 View클래스를 상속 받았으므로 get함수와 post함수를 각각 만들어줘야한다.
+    # 프로필 편집에서 보여주기위한 get 메소드
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)  # 로그인중인 사용자 객체를 얻어옴
+        user_form = UserForm(initial={
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
+
+        if hasattr(user, 'profile'):  # user가 profile을 가지고 있으면 True, 없으면 False (회원가입을 한다고 profile을 가지고 있진 않으므로)
+            profile = user.profile
+            profile_form = ProfileForm(initial={
+                'nickname': profile.nickname,
+                'profile_photo': profile.profile_photo,
+            })
+        else:
+            profile_form = ProfileForm()
+
+        return render(request, 'lens/profile_update.html', {"user_form": user_form, "profile_form": profile_form})
+
+    # 프로필 편집에서 실제 수정(저장) 버튼을 눌렀을 때 넘겨받은 데이터를 저장하는 post 메소드
+    def post(self, request):
+        u = User.objects.get(id=request.user.pk)        # 로그인중인 사용자 객체를 얻어옴
+        user_form = UserForm(request.POST, instance=u)  # 기존의 것의 업데이트하는 것 이므로 기존의 인스턴스를 넘겨줘야한다. 기존의 것을 가져와 수정하는 것
+
+        # User 폼
+        if user_form.is_valid():
+            user_form.save()
+
+        if hasattr(u, 'profile'):
+            profile = u.profile
+            profile_form = ProfileForm(request.POST, request.FILES, instance=profile) # 기존의 것 가져와 수정하는 것
+        else:
+            profile_form = ProfileForm(request.POST, request.FILES) # 새로 만드는 것
+
+        # Profile 폼
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False) # 기존의 것을 가져와 수정하는 경우가 아닌 새로 만든 경우 user를 지정해줘야 하므로
+            profile.user = u
+            profile.save()
+
+        return redirect('lens:profile', pk=request.user.pk) # 수정된 화면 보여주기
+
+
+class CreateUserView(CreateView): # 제네릭뷰에서 CreateView는 폼이나 모델과 연결되어 새로운 데이터를 넣을 때 사용됨.
+    template_name = 'registration/signup.html'
+    form_class = CreateUserForm
+    success_url = reverse_lazy('signup_done')
+    # reverse 말고 reverse_lazy를 사용하는 이유는 제네릭뷰를 사용할 때 타이밍 문제 때문에
+
+
+class RegisteredView(TemplateView):
+    template_name = 'registration/signup_done.html'
+
+
+# @csrf_protect
+# def password_reset(request, is_admin_site=False,
+#                    template_name='registration/password_reset_form.html',
+#                    email_template_name='registration/password_reset_email.html',
+#                    subject_template_name='registration/password_reset_subject.txt',
+#                    password_reset_form=PasswordResetForm,
+#                    token_generator=default_token_generator,
+#                    post_reset_redirect=None,
+#                    from_email=None,
+#                    current_app=None,
+#                    extra_context=None,
+#                    html_email_template_name=None):
+#     if post_reset_redirect is None:
+#         post_reset_redirect = reverse('password_reset_done')
+#     else:
+#         post_reset_redirect = resolve_url(post_reset_redirect)
+#     if request.method == "POST":
+#         form = password_reset_form(request.POST)
+#         if form.is_valid():
+#             opts = {
+#                 'use_https': request.is_secure(),
+#                 'token_generator': token_generator,
+#                 'from_email': from_email,
+#                 'email_template_name': email_template_name,
+#                 'subject_template_name': subject_template_name,
+#                 'request': request,
+#                 'html_email_template_name': html_email_template_name,
+#             }
+#             if is_admin_site:
+#                 opts = dict(opts, domain_override=request.get_host())
+#             form.save(**opts)
+#             return HttpResponseRedirect(post_reset_redirect)
+#     else:
+#         form = password_reset_form()
+#     context = {
+#         'form': form,
+#         'title': _('Password reset'),
+#     }
+#     if extra_context is not None:
+#         context.update(extra_context)
+#     return TemplateResponse(request, template_name, context,
+#                             current_app=current_app)
+#
+#
+# def password_reset_done(request,
+#                         template_name='registration/password_reset_done.html',
+#                         current_app=None, extra_context=None):
+#     context = {
+#         'title': _('Password reset successful'),
+#     }
+#     if extra_context is not None:
+#         context.update(extra_context)
+#     return TemplateResponse(request, template_name, context,
+#                             current_app=current_app)
+#
+#
+# # Doesn't need csrf_protect since no-one can guess the URL
+# @sensitive_post_parameters()
+# @never_cache
+# def password_reset_confirm(request, uidb64=None, token=None,
+#                            template_name='registration/password_reset_confirm.html',
+#                            token_generator=default_token_generator,
+#                            set_password_form=SetPasswordForm,
+#                            post_reset_redirect=None,
+#                            current_app=None, extra_context=None):
+#     """
+#     View that checks the hash in a password reset link and presents a
+#     form for entering a new password.
+#     """
+#     UserModel = get_user_model()
+#     assert uidb64 is not None and token is not None  # checked by URLconf
+#     if post_reset_redirect is None:
+#         post_reset_redirect = reverse('password_reset_complete')
+#     else:
+#         post_reset_redirect = resolve_url(post_reset_redirect)
+#     try:
+#         uid = urlsafe_base64_decode(uidb64)
+#         user = UserModel._default_manager.get(pk=uid)
+#     except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+#         user = None
+#
+#     if user is not None and token_generator.check_token(user, token):
+#         validlink = True
+#         title = _('Enter new password')
+#         if request.method == 'POST':
+#             form = set_password_form(user, request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 return HttpResponseRedirect(post_reset_redirect)
+#         else:
+#             form = set_password_form(user)
+#     else:
+#         validlink = False
+#         form = None
+#         title = _('Password reset unsuccessful')
+#     context = {
+#         'form': form,
+#         'title': title,
+#         'validlink': validlink,
+#     }
+#     if extra_context is not None:
+#         context.update(extra_context)
+#     return TemplateResponse(request, template_name, context,
+#                             current_app=current_app)
+#
+#
+# def password_reset_complete(request,
+#                             template_name='registration/password_reset_complete.html',
+#                             current_app=None, extra_context=None):
+#     context = {
+#         'login_url': resolve_url(settings.LOGIN_URL),
+#         'title': _('Password reset complete'),
+#     }
+#     if extra_context is not None:
+#         context.update(extra_context)
+#     return TemplateResponse(request, template_name, context,
+#                             current_app=current_app)
---
+++ mysite/urls.py
@@ -0,0 +1,48 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+from lens import views as lens_views
+urlpatterns = [
+    path('', login_required(lens_views.IndexView.as_view()), name='root'),
+    path('admin/', admin.site.urls),
+    path('lens/', include('lens.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', lens_views.CreateUserView.as_view(), name='signup'),
+    path('accounts/signup/success/', lens_views.RegisteredView.as_view(), name='signup_done'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
+    ]
+
+# 첫번째 인자 : 어떤 URL을 정적으로 추가할래?, 두번쨰 인자 : 실제는 어디에 있는데?
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
