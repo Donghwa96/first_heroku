8d9f1198ae433c69ab4489f946d7cb93b15bf3a3
Donghwa96 <ensk960405@gmail.com>
2019-07-29 23:26:55 +0900

    requirements.txt 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ if DEBUG:
 
 INSTALLED_APPS = [
     'lens',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
