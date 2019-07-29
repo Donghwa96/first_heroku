ba3cf45d016b6a81ceb650c95774677c3bb6e24b
Donghwa96 <ensk960405@gmail.com>
2019-07-30 00:00:05 +0900

    middlewhare debuging

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,6 @@ if DEBUG:
 
 INSTALLED_APPS = [
     'lens',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
